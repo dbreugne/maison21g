28d2168d4a9e03872f5b4e9bd79ab152dc63f0cb
libu <libukoshym@gmail.com>
2020-06-24 10:25:36 +0530

    POS Section-POS Screen,Backend Orderline,POS Receipt

---
+++ dev_pos_line_section/__manifest__.py
@@ -0,0 +1,28 @@
+
+
+{
+    'name': 'POS Line Section', 
+    'version': '13.0.1.1',
+    'sequence': 1, 
+    'category': 'Point Of Sale', 
+    'description': 
+        """ 
+        Add section in POS cart.
+        Print receipt based on section.
+    """,
+    'summary': 'Add Section in POS Cart.',
+    'author': '', 
+    'website': '',
+    'depends': ['point_of_sale'],
+    'data': [
+        'data/data.xml',
+        'views/pos_models.xml',
+        'views/template.xml',
+    ],
+    'qweb': ['static/src/xml/pos.xml'],
+    'images': ['images/main_screenshot.png'],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
+
