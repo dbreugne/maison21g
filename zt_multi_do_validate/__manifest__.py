f4b0c35d3e31e554ec3f6e6eeca74c067d1e0ae0
libu <libukoshym@gmail.com>
2020-07-03 15:01:23 +0530

    Validate Multiple DO From Picking

---
+++ zt_multi_do_validate/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Multiple Do Validate From Tree View',
+    'version': '10.0',
+    'author': '',
+    'summary': 'Confirm Do',
+    'category': 'Stock',
+    'depends': ['stock'],
+    'data': [
+        'wizard/do_wiz_view.xml',
+        'views/stock_validate_view.xml',
+
+    ],
+    'description': """
+       
+    """,
+    'images': ['static/img/main.png'],
+    'auto_install': False,
+    'installable': True,
+}
+
