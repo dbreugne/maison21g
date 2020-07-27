28f0fedc39d7ab35ac0cf1e67aabfdec934d71ba
libu <libukoshym@gmail.com>
2020-07-27 10:10:40 +0530

    Stock Report with Zero Quantity Items

---
+++ zt_stock_report/__manifest__.py
@@ -0,0 +1,11 @@
+
+{
+    "name": "Stock Quantity Details Location",
+    "summary": "Provides stock quantity by location",
+    "version": "13.0.1.1.0",
+    "license": "AGPL-3",
+    "author": "ZT",
+    "website": "",
+    "depends": ["stock"],
+    "data": ["wizards/stock_quantity_history.xml"],
+}
