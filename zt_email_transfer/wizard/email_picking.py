ec64136e41a12a32b2b37ca336bc71d6cbd57fb1
libu <libukoshym@gmail.com>
2020-07-28 15:31:03 +0530

    Internal Transfer Report by Email Commit

---
+++ zt_email_transfer/wizard/email_picking.py
@@ -0,0 +1,39 @@
+
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError
+
+
+class MultiDo(models.Model):
+
+    _inherit = 'stock.picking'
+
+    def action_send_mail_transfer(self):
+        self.ensure_one()
+        ir_model_data = self.env['ir.model.data']
+        try:
+            template_id = ir_model_data.get_object_reference('zt_email_transfer', 'stock_transfer_email_template')[1]
+        except ValueError:
+            template_id = False
+        try:
+            compose_form_id = ir_model_data.get_object_reference('mail', 'email_compose_message_wizard_form')[1]
+        except ValueError:
+            compose_form_id = False
+        ctx = {
+            'default_model': 'stock.picking',
+            'default_res_id': self.ids[0],
+            'default_use_template': bool(template_id),
+            'default_template_id': template_id,
+            'default_composition_mode': 'comment',
+            'mark_so_as_sent': True,
+            'force_email': True
+        }
+        return {
+            'type': 'ir.actions.act_window',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'mail.compose.message',
+            'views': [(compose_form_id, 'form')],
+            'view_id': compose_form_id,
+            'target': 'new',
+            'context': ctx,
+        }
