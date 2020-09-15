cb42be481f9841ede9a7d3ba646bc1e09508ffde
libu <libukoshym@gmail.com>
2020-09-15 07:43:50 +0530

    Multiple MRP Orders For Quantities Less than 1

---
+++ zt_mrp_multi_production/wizard/multi_mrp_order.py
@@ -0,0 +1,143 @@
+from odoo import api, models,fields,_
+from odoo.exceptions import UserError,ValidationError
+from datetime import datetime, timedelta
+
+class MultiMRP(models.Model):
+
+    _name = "multi.mrp"
+
+    @api.model
+    def _get_default_picking_type(self):
+        company_id = self.env.context.get('default_company_id', self.env.company.id)
+        return self.env['stock.picking.type'].search([
+            ('code', '=', 'mrp_operation'),
+            ('warehouse_id.company_id', '=', company_id),
+        ], limit=1).id
+
+    name=fields.Char('Name',required=True)
+    multimrp_order_line_ids = fields.One2many('mrpmulti.lines','multi_batch_id','Batch MRP Orders',help="Here listed only on-hand quantity of product less than or equal to zero.")
+    picking_type_id = fields.Many2one(
+        'stock.picking.type', 'Picking Type',
+        default=_get_default_picking_type, required=True)
+
+
+
+    def generate_mrp_batch(self):
+        if len(self.multimrp_order_line_ids)==0:
+            raise ValidationError(_('Cannot Create a Order without Line'))
+        for record in self.multimrp_order_line_ids:
+            vals={
+                'product_id':record.product_id.id,
+                'initial_qty':record.qty_produce,
+                'location_dest_id':record.location_id.id,
+                'date_stat_wo':record.schedule_date,
+                'bom_id':record.bom_id.id,
+                'product_uom_id':record.product_uom_id.id,
+                'origin':'Batch Processing'+" "+self.name,
+                'product_qty':record.qty_produce
+            }
+            production_obj=self.env['mrp.production'].create(vals)
+            production_obj._onchange_move_raw()
+            # production_obj.action_confirm()
+
+    @api.model
+    def default_get(self, fields):
+        res = super(MultiMRP, self).default_get(fields)
+        company_id = self.env.context.get('default_company_id', self.env.company.id)
+
+        mrp_lines = []
+        product_rec = self.env['mrp.bom'].search([])
+        for all in product_rec:
+            qty_hand = all.product_tmpl_id.with_context({'location': all.product_tmpl_id.location_id}).qty_available
+            if qty_hand <= 0:
+                for pro in all:
+                    product_id = self.env['product.product'].search([('product_tmpl_id', '=', all.product_tmpl_id.id)])
+                    line = (0, 0, {
+                        'product_id': product_id.id,
+                        'product_uom_id': self.env['uom.uom'].search([], limit=1, order='id').id,
+                        'schedule_date':datetime.today(),
+                        'qty_hand': all.product_tmpl_id.with_context(
+                            {'location': all.product_tmpl_id.location_id}).qty_available,
+                        'bom_id': self.env['mrp.bom']._bom_find(product=product_id,
+                                                                picking_type=self.picking_type_id,
+                                                                company_id=all.company_id.id, bom_type='normal').id,
+                        'picking_type_id':self.env['stock.picking.type'].search([
+                            ('code', '=', 'mrp_operation'),
+                            ('warehouse_id.company_id', '=', company_id),
+                        ], limit=1).id
+                    })
+                    mrp_lines.append(line)
+                    res.update({
+                        'multimrp_order_line_ids': mrp_lines,
+                    })
+        return res
+
+
+class MultiMRPLine(models.Model):
+
+    _name = 'mrpmulti.lines'
+
+    @api.model
+    def _get_default_picking_type(self):
+        company_id = self.env.context.get('default_company_id', self.env.company.id)
+        return self.env['stock.picking.type'].search([
+            ('code', '=', 'mrp_operation'),
+            ('warehouse_id.company_id', '=', company_id),
+        ], limit=1).id
+
+    def _get_default_product_uom_id(self):
+        return self.env['uom.uom'].search([], limit=1, order='id').id
+
+    @api.model
+    def _get_default_picking_type(self):
+        company_id = self.env.context.get('default_company_id', self.env.company.id)
+        return self.env['stock.picking.type'].search([
+            ('code', '=', 'mrp_operation'),
+            ('warehouse_id.company_id', '=', company_id),
+        ], limit=1).id
+
+    product_uom_id = fields.Many2one(
+        'uom.uom', 'Product Unit of Measure',
+        default=_get_default_product_uom_id, required=True,
+        help="Unit of Measure (Unit of Measure) is the unit of measurement for the inventory control")
+
+    multi_batch_id = fields.Many2one('multi.mrp')
+    product_id = fields.Many2one(
+        'product.product', 'Product',
+        domain="[('bom_ids', '!=', False), ('bom_ids.active', '=', True), ('bom_ids.type', '=', 'normal'), ('type', 'in', ['product', 'consu'])]",
+        required=True)
+
+    location_id = fields.Many2one('stock.location','Location',domain="[('usage','=','internal')]",required=True)
+    qty_produce =fields.Float('Qty Produced')
+    varity_id=fields.Many2one('product.product','variant')
+    schedule_date =fields.Date('Schedule Date')
+    qty_hand = fields.Float('Quantity On Hand',)
+    bom_id = fields.Many2one(
+        'mrp.bom', 'Bill of Material',
+        help="Bill of Materials allow you to define the list of required raw materials to make a finished product.")
+    company_id = fields.Many2one(
+        'res.company', 'Company', default=lambda self: self.env.company,
+        index=True, required=True)
+    product_qty = fields.Float('Qty')
+    picking_type_id = fields.Many2one(
+        'stock.picking.type', 'Picking Type',
+        default=_get_default_picking_type, required=True)
+
+    @api.onchange('product_id', 'picking_type_id', 'company_id')
+    def onchange_product_id(self):
+        self.qty_hand = self.product_id.with_context({'location': self.product_id.location_id}).qty_available
+        """ Finds UoM of changed product. """
+        if not self.product_id:
+            self.bom_id = False
+        else:
+            bom = self.env['mrp.bom']._bom_find(product=self.product_id, picking_type=self.picking_type_id,
+                                                company_id=self.company_id.id, bom_type='normal')
+            if bom:
+                self.bom_id = bom.id
+                self.product_qty = self.bom_id.product_qty
+                self.product_uom_id = self.bom_id.product_uom_id.id
+            else:
+                self.bom_id = False
+                self.product_uom_id = self.product_id.uom_id.id
+            return {'domain': {'product_uom_id': [('category_id', '=', self.product_id.uom_id.category_id.id)]}}
+
