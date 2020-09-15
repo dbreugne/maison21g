3daf1645061c918807278ea0b5e5d6ff8789b11e
libu <libukoshym@gmail.com>
2020-09-15 07:43:50 +0530

    Multiple MRP Orders For Quantities Less than 1

---
+++ zt_mrp_multi_production/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'ZT Multi MRP Order',
+    'version': '10.0.2.0',
+    'summary': 'Multi Manufacturing Order',
+    'description': """
+   '.
+    """,
+    'author': '',
+    'company': '',
+    'website': "",
+    'category': 'Manufacturing',
+    'depends': ['mrp'],
+    'data': [
+        'security/ir.model.access.csv',
+        'wizard/multi_order_view.xml',
+    ],
+    'demo': [],
+    'images': ['static/description/icon.png'],
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': False
+}
