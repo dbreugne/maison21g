c5263a00ba7b947f6881882db18aaaf6d5447c23
libu <libukoshym@gmail.com>
2021-01-21 11:16:10 +0530

    Delivery Report with PO number,total quantity Commit

---
+++ delivery_report_modifications/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'ZT Delivery Report Modification',
+    'version': '1.0.0',
+    'summary': 'Delivery Report Modification',
+    'description': """
+        Delivery Report Modification 
+    """,
+    'depends': [
+        'base','stock', 'purchase'],
+    'data': [
+        'views/delivery_report.xml',
+        'views/delivery_picking_view.xml'
+
+      
+    ]
+}
