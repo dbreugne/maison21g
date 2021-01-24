5bdb8b760b2d61180de5c75fe295d962a959b4a2
libu <libukoshym@gmail.com>
2021-01-24 21:02:42 +0530

    Delivery Report with PO number ,based on sales Order

--- delivery_report_modifications/models/stock_picking.py
+++ delivery_report_modifications/models/stock_picking.py
@@ -5,3 +5,7 @@ class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
     po_number = fields.Many2one("purchase.order", string="PO")
+
+
+    def button_print_delivery(self):
+        return self.env.ref('delivery_report_modifications.action_report_delivery').report_action(self)
