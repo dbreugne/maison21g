4a785c7c41c2f6642f536f2330b88f3aaf8d3613
libu <libukoshym@gmail.com>
2021-01-24 21:35:21 +0530

    Delivery Report with PO number ,based on sales Order

--- delivery_report_modifications/models/stock_picking.py
+++ delivery_report_modifications/models/stock_picking.py
@@ -4,7 +4,7 @@ from odoo import api, fields, models
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
-    po_number = fields.Many2one("purchase.order", string="PO")
+    po_number = fields.Many2one("purchase.order", string="PO Num")
 
 
     def button_print_delivery(self):
