a6380c2ae77412c1e6e68b81dfaad201ac8f9305
libu <libukoshym@gmail.com>
2021-01-24 21:02:42 +0530

    Delivery Report with PO number ,based on sales Order

--- delivery_report_modifications/__manifest__.py
+++ delivery_report_modifications/__manifest__.py
@@ -10,7 +10,8 @@
         'base','stock', 'purchase'],
     'data': [
         'views/delivery_report.xml',
-        'views/delivery_picking_view.xml'
+        'views/delivery_picking_view.xml',
+        'views/delivery_button_print_report.xml'
 
       
     ]
