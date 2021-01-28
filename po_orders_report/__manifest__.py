6ada271f1bd237eebdd571ced362259421055e63
libu <libukoshym@gmail.com>
2021-01-28 22:47:36 +0530

    POS Orders Print Invoice Commit

---
+++ po_orders_report/__manifest__.py
@@ -0,0 +1,27 @@
+
+{
+    'name': 'POS Orders Report - Maison',
+    'version': '13.0.0',
+    'sequence': 1,
+    'category': '',
+    'description':
+        """
+    """,
+    'summary': 'POS Order print',
+    'author': 'ZT-DEV',
+    'depends': ['point_of_sale'],
+    'data': [
+        'views/pos_report_invoice.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'css': [],
+    'qweb': [],
+    'js': [],
+    'images': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
