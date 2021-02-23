e0327e14c63c5a98711373fb959cb08045a297e2
libu <libukoshym@gmail.com>
2021-02-23 11:45:43 +0530

    POS Export Order empty cell data commit

--- zt_pos_orders_report/wizard/zt_report_pos_wizard.py
+++ zt_pos_orders_report/wizard/zt_report_pos_wizard.py
@@ -98,9 +98,9 @@ class ZtReportPos(models.TransientModel):
             col_index = False
             order_data_list = []
             for index, lines in enumerate(order_line.lines, start=1):
-                if lines.product_id.is_pos_master:
+                if lines.product_id.lines.name:
                     col_index = index
-                if col_index and not lines.product_id and lines.product_id.name:
+                if col_index and not lines.name:
                     new_column_data[col_index] += [lines.product_id.name]
                 # if col_index and not lines.product_id and lines.product_id.name:
                 #     combination_column_data[col_index] += [lines.product_id.name]
