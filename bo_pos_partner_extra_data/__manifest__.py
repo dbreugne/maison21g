30b33533dcf995852acc73db80229671a0e126e6
libu <libukoshym@gmail.com>
2021-03-05 21:15:27 +0530

    Partner Data From POS Screen commit

---
+++ bo_pos_partner_extra_data/__manifest__.py
@@ -0,0 +1,19 @@
+
+
+{
+    'name': 'ZT Pos Partner Doc',
+    'summary': 'zt pos data',
+    'description': """zt pos data.""",
+    'category': 'Point Of Sale',
+    'version': '1.0',
+    'website': 'http://www.zt.com/',
+    'author': 'ZTR',
+    'depends': ['point_of_sale', 'bo_partner_extra_data'],
+    'data': [
+        'views/point_of_sale.xml',
+    ],
+    'qweb': [
+        'static/src/xml/pos.xml'
+    ],
+    'application': True,
+}
