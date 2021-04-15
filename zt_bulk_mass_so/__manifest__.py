ec3bf7c1519278fec9679addde458d444787dd6b
libu <libukoshym@gmail.com>
2021-04-15 16:51:58 +0530

    Mass Confirm SO

---
+++ zt_bulk_mass_so/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0).
+
+{
+    'name': 'Sale Mass Confirm ',
+    'version': '13.0.1.0.0',
+    'category': 'Generic Modules/Others',
+    'description': '',
+    'summary': '',
+    'author': 'ZTDEV',
+    'maintainer': 'ZTDEV',
+    'website': 'http://consult.erpits.com',
+    'support': 'sales.erptech@gmail.com',
+    'license': 'LGPL-3',
+    'depends': [
+        'sale',
+    ],
+    'data': [
+        'wizards/sale_view.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
