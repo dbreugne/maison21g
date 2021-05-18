edc356cb4f74536b33cab6383d46de4427c1ed81
libu <libukoshym@gmail.com>
2021-05-18 16:42:02 +0530

    Automate Csv generator by cron Format Issue commit

--- zt_sale_csv_creator/models/sale.py
+++ zt_sale_csv_creator/models/sale.py
@@ -1,3 +1,5 @@
+import io
+
 from odoo import api, fields, models
 from datetime import datetime, timedelta, date
 import csv
@@ -15,55 +17,77 @@ class SaleOrder(models.Model):
         delta = today - start_date
         return "MBSSH{seq}".format(seq=delta.days)
 
-    def create_sale_order_csv(self, env=None):
-        sequence = self.get_sequence()
-        directory = self.env.company.csv_folder or "/tmp"
-        try:
-            if not os.path.isdir(directory):
-                os.makedirs(directory)
-        except:
-            raise
-        current_date_time = datetime.now()
-        last_hour_date_time = current_date_time - timedelta(hours=1)
-        tz = self.env.user.tz
-        hour_date_time = fields.Datetime.context_timestamp(self.with_context(tz=tz), current_date_time - timedelta(minutes=25))
-        domain = [("create_date", "<=", fields.Datetime.to_string(current_date_time)),
-                  ("create_date", ">", fields.Datetime.to_string(last_hour_date_time))]
-        sale_orders = self.search(domain)
+    def get_csv_line_by_date_time(self, hour_date_time, tz, current_date_time):
+        start_time = current_date_time.replace(minute=00, second=00)
+        end_time = current_date_time.replace(minute=59, second=59)
+        domain = [("create_date", ">=", fields.Datetime.to_string(start_time)),
+                  ("create_date", "<=", fields.Datetime.to_string(end_time))]
+        sale_orders = self.with_context(tz=tz).search(domain)
         amount_tax = 0.0
         amount_total = 0.0
         for indx, item in enumerate(sale_orders, start=1):
             amount_tax += item.amount_tax
             amount_total += item.amount_total
-        row = [sequence,
+        row = [self.get_sequence(),
                hour_date_time.strftime("%Y-%m-%d"),
                hour_date_time.strftime("%H"),
                amount_tax,
                amount_total,
                len(sale_orders)
                ]
-        file_name = "{directory}/mbssh_{date_time}.csv".format(directory=directory,
-                                                               date_time=fields.Date.to_string(current_date_time))
+        return row
+
+    def create_sale_order_csv(self, env=None):
+        directory = self.env.company.csv_folder or "/tmp"
+        try:
+            if not os.path.isdir(directory):
+                os.makedirs(directory)
+        except:
+            raise
+        current_date_time = datetime.now() - timedelta(minutes=35)
+        tz = self.user_id.tz
+        hour_date_time = fields.Datetime.context_timestamp(self.with_context(tz=tz), current_date_time)
+        current_row = self.get_csv_line_by_date_time(hour_date_time, tz, current_date_time)
         attachment_name = "mbssh_{date_time}.csv".format(
                 date_time=fields.Date.to_string(current_date_time))
         attachment = self.env["ir.attachment"].sudo().search([('name', '=', attachment_name)],
                                                              limit=1)
         if attachment:
-            content = attachment.unlink()
-            # writer = csv.writer(content, delimiter=',',
-            #                     quotechar='|', quoting=csv.QUOTE_MINIMAL)
-            # writer.writerow(row)
-        # else:
-        with open(file_name, 'a', newline='') as file:
-            writer = csv.writer(file, delimiter=',',
-                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
-            writer.writerow(row)
-        self.env['ir.attachment'].create({
-            'datas': base64.b64encode(open(file_name).read().encode("utf-8")),
-            'name': attachment_name,
-            'type': 'binary',
-            'public': False
-        })
+            content = base64.b64decode(attachment.datas.decode('utf-8')).decode()
+            lines = content.splitlines()
+            last_line = lines[-1]
+            last_hour = int(last_line.split(",")[2]) if last_line and len(last_line)>3 else 0
+            total_to_update = hour_date_time.hour - last_hour
+            if total_to_update == 0 and lines:
+                lines.pop(-1)
+            elif total_to_update > 1:
+                for hour in range(total_to_update, 0, -1):
+                    hour_date_to_get = hour_date_time - timedelta(hours=hour)
+                    if hour < last_hour:
+                        missed_row = self.get_csv_line_by_date_time(hour_date_to_get, tz, current_date_time)
+                        lines.append(",".join(str(x) for x in missed_row))
+            lines.append(",".join(str(x) for x in current_row))
+            joined_lines = "\n".join(lines) + "\n"
+            attachment.write(
+                {"datas": base64.b64encode(joined_lines.encode("utf-8")), "mimetype": attachment.mimetype}
+            )
+        else:
+            lines = []
+            for hour in range(hour_date_time.hour, 0, -1):
+                domain_hour = current_date_time - timedelta(hours=hour)
+                time_to_get = fields.Datetime.context_timestamp(self.with_context(tz=tz), domain_hour)
+                if hour <= hour_date_time.hour:
+                    missed_row = self.get_csv_line_by_date_time(time_to_get, tz, domain_hour)
+                    lines.append(",".join(str(x) for x in missed_row))
+            lines.append(",".join(str(x) for x in current_row))
+            joined_lines = "\n".join(lines) + "\n"
+            self.env['ir.attachment'].create({
+                'datas': base64.b64encode(joined_lines.encode("utf-8")),
+                'mimetype': "text/csv",
+                'name': attachment_name,
+                'type': 'binary',
+                'public': False
+            })
         """
         Remove all old files (on local server) in case this is configured..
         """
