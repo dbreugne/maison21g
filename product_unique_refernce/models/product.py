210639bbf97759d55a2f2aba20403dfe9a8ed5f9
libu <libukoshym@gmail.com>
2021-10-15 10:13:29 +0530

    Unique Product Internal Ref copy changes -commit

--- product_unique_refernce/models/product.py
+++ product_unique_refernce/models/product.py
@@ -5,15 +5,17 @@ from odoo.exceptions import ValidationError
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    # _sql_constraints = [('unique_default_code', 'UNIQUE(default_code)', 'Internal Reference Code must be unique')]
+    default_code = fields.Char('Internal Reference', index=True, copy=False)
+
+    _sql_constraints = [('unique_default_code', 'UNIQUE(default_code)', 'Internal Reference Code must be unique')]
     # @api.model
     # def create(self, vals):
     #     res=
-    @api.constrains('default_code')
-    def _check_default_code(self):
-        code = self.search([('default_code', '=', self.default_code)])
-        if len(code) > 1:
-            raise ValidationError(_("Internal Reference Code must be unique"))
+    # @api.constrains('default_code')
+    # def _check_default_code(self):
+    #     code = self.search([('default_code', '=', self.default_code)])
+    #     if len(code) > 1:
+    #         raise ValidationError(_("Internal Reference Code must be unique"))
 
 class ProductProduct(models.Model):
     _inherit = "product.product"
