2d16d312a7a0291884a3f35a904d4d67a54dea23
libu <libukoshym@gmail.com>
2021-10-15 10:24:36 +0530

    Unique Product Internal Ref copy changes -commit

--- product_unique_refernce/models/product.py
+++ product_unique_refernce/models/product.py
@@ -11,28 +11,38 @@ class ProductTemplate(models.Model):
     # @api.model
     # def create(self, vals):
     #     res=
-    # @api.constrains('default_code')
-    # def _check_default_code(self):
-    #     code = self.search([('default_code', '=', self.default_code)])
-    #     if len(code) > 1:
-    #         raise ValidationError(_("Internal Reference Code must be unique"))
+    @api.constrains('default_code')
+    def _check_default_code(self):
+        code = self.search([('default_code', '=', self.default_code)])
+        if len(code) > 1:
+            raise ValidationError(_("Internal Reference Code must be unique"))
+
+    def copy(self, default=None):  # pylint: disable=W0622
+
+        if not default:
+            default = {}
+
+        default[
+            'default_code'] = self.default_code and\
+            self.default_code + ' (copy)' or False
 
 class ProductProduct(models.Model):
     _inherit = "product.product"
 
     default_code = fields.Char('Internal Reference', index=True,copy=False)
-    # def copy(self, default=None):  # pylint: disable=W0622
-    #
-    #     if not default:
-    #         default = {}
-    #
-    #     default[
-    #         'default_code'] = self.default_code and\
-    #         self.default_code + ' (copy)' or False
-    #
-    #     return super(ProductProduct, self).copy(default=default)
+    def copy(self, default=None):  # pylint: disable=W0622
+
+        if not default:
+            default = {}
+
+        default[
+            'default_code'] = self.default_code and\
+            self.default_code + ' (copy)' or False
+
+        return super(ProductProduct, self).copy(default=default)
 
     # _sql_constraints = [
+
     #     ('default_code_unique', 'unique (default_code)',
     #      'The code of Product must be unique !'),
     # ]
