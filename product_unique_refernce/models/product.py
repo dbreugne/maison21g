5de2c4c095806b10dffaf4b17ee2f19a2aba1ada
libu <libukoshym@gmail.com>
2021-10-15 22:18:18 +0530

    Unique Product Internal Ref copy Issue Fix -commit

--- product_unique_refernce/models/product.py
+++ product_unique_refernce/models/product.py
@@ -8,15 +8,8 @@ class ProductTemplate(models.Model):
     default_code = fields.Char('Internal Reference', index=True, copy=False)
 
     _sql_constraints = [('unique_default_code', 'UNIQUE(default_code)', 'Internal Reference Code must be unique')]
-    # @api.model
-    # def create(self, vals):
-    #     res=
-    @api.constrains('default_code')
-    def _check_default_code(self):
-        code = self.search([('default_code', '=', self.default_code)])
-        if len(code) > 1:
-            raise ValidationError(_("Internal Reference Code must be unique"))
 
+    @api.returns('self', lambda value: value.id)
     def copy(self, default=None):  # pylint: disable=W0622
 
         if not default:
@@ -25,6 +18,8 @@ class ProductTemplate(models.Model):
         default[
             'default_code'] = self.default_code and\
             self.default_code + ' (copy)' or False
+        product = super(ProductTemplate, self).copy(default)
+        return product
 
 class ProductProduct(models.Model):
     _inherit = "product.product"
@@ -40,12 +35,5 @@ class ProductProduct(models.Model):
             self.default_code + ' (copy)' or False
 
         return super(ProductProduct, self).copy(default=default)
-
-    # _sql_constraints = [
-
-    #     ('default_code_unique', 'unique (default_code)',
-    #      'The code of Product must be unique !'),
-    # ]
-
 
 
