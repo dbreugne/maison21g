4f6383bfd1fcc582a35f1939fdcddb9d7185d491
libu <libukoshym@gmail.com>
2021-10-15 23:42:39 +0530

    Unique Product Internal Ref copy Issue Fix -commit

--- product_unique_refernce/models/product.py
+++ product_unique_refernce/models/product.py
@@ -1,4 +1,4 @@
-from odoo import models,fields,api,_
+from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError
 
 
@@ -8,6 +8,12 @@ class ProductTemplate(models.Model):
     default_code = fields.Char('Internal Reference', index=True, copy=False)
 
     _sql_constraints = [('unique_default_code', 'UNIQUE(default_code)', 'Internal Reference Code must be unique')]
+
+    @api.constrains('default_code')
+    def _check_default_code(self):
+        code = self.search([('default_code', '=', self.default_code)])
+        if len(code) > 1:
+            raise ValidationError(_("Internal Reference Code must be unique"))
 
     @api.returns('self', lambda value: value.id)
     def copy(self, default=None):  # pylint: disable=W0622
@@ -21,10 +27,12 @@ class ProductTemplate(models.Model):
         product = super(ProductTemplate, self).copy(default)
         return product
 
+
 class ProductProduct(models.Model):
     _inherit = "product.product"
 
-    default_code = fields.Char('Internal Reference', index=True,copy=False)
+    default_code = fields.Char('Internal Reference', index=True, copy=False)
+
     def copy(self, default=None):  # pylint: disable=W0622
 
         if not default:
