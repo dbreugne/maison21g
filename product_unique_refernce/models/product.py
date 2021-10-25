4b6e3214f7dcb8294871915c578ca42fd189542a
libu <libukoshym@gmail.com>
2021-10-25 16:32:09 +0530

    Unique Product Internal Reference  Issue Fix Commit

--- product_unique_refernce/models/product.py
+++ product_unique_refernce/models/product.py
@@ -1,5 +1,5 @@
 from odoo import models, fields, api, _
-from odoo.exceptions import ValidationError
+from odoo.exceptions import ValidationError, UserError
 
 
 class ProductTemplate(models.Model):
@@ -7,13 +7,13 @@ class ProductTemplate(models.Model):
 
     default_code = fields.Char('Internal Reference', index=True, copy=False)
 
-    _sql_constraints = [('unique_default_code', 'UNIQUE(default_code)', 'Internal Reference Code must be unique')]
+    # _sql_constraints = [('unique_default_code', 'UNIQUE(default_code)', 'Internal Reference Code must be unique')]
 
-    # @api.constrains('default_code')
-    # def _check_default_code(self):
-    #     code = self.search([('default_code', '=', self.default_code)])
-    #     if len(code) > 1:
-    #         raise ValidationError(_("Internal Reference Code must be unique"))
+    @api.constrains('default_code')
+    def _check_default_code(self):
+        code = self.search([('default_code', '=', self.default_code)])
+        if len(code) > 1:
+            raise UserError(_("Internal Reference Code must be unique"))
 
     @api.returns('self', lambda value: value.id)
     def copy(self, default=None):  # pylint: disable=W0622
@@ -43,5 +43,4 @@ class ProductProduct(models.Model):
         #     self.default_code + ' (copy)' or False
 
         return super(ProductProduct, self).copy(default=default)
-
 
