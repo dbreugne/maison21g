f01123886a0ebff29bad41c9806b7d6deda06203
libu <libukoshym@gmail.com>
2021-10-25 17:18:47 +0530

    Unique Product Internal Reference  Issue Fix Commit

--- product_unique_refernce/models/product.py
+++ product_unique_refernce/models/product.py
@@ -7,40 +7,16 @@ class ProductTemplate(models.Model):
 
     default_code = fields.Char('Internal Reference', index=True, copy=False)
 
-    # _sql_constraints = [('unique_default_code', 'UNIQUE(default_code)', 'Internal Reference Code must be unique')]
-
     @api.constrains('default_code')
     def _check_default_code(self):
-        code = self.search([('default_code', '=', self.default_code)])
-        if len(code) > 1:
-            raise UserError(_("Internal Reference Code must be unique"))
-
-    @api.returns('self', lambda value: value.id)
-    def copy(self, default=None):  # pylint: disable=W0622
-
-        if not default:
-            default = {}
-
-        # default[
-        #     'default_code'] = self.default_code and\
-        #     self.default_code + ' (copy)' or False
-        product = super(ProductTemplate, self).copy(default)
-        return product
+        if self.default_code:
+            code = self.search([('default_code', '=', self.default_code)])
+            if len(code) > 1:
+                raise UserError(_("Internal Reference Code must be unique"))
 
 
 class ProductProduct(models.Model):
     _inherit = "product.product"
 
     default_code = fields.Char('Internal Reference', index=True, copy=False)
-
-    def copy(self, default=None):  # pylint: disable=W0622
-
-        if not default:
-            default = {}
-
-        # default[
-        #     'default_code'] = self.default_code and\
-        #     self.default_code + ' (copy)' or False
-
-        return super(ProductProduct, self).copy(default=default)
 
