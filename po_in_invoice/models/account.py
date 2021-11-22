890e5da8d3b3a3a3dc11d5d921f0cc7f8a856664
libu <libukoshym@gmail.com>
2021-11-22 22:25:36 +0530

    PO added on Journal Entries -commit

--- po_in_invoice/models/account.py
+++ po_in_invoice/models/account.py
@@ -1,8 +1,8 @@
 from odoo import api, fields, models
 
 
-class AccountInvoice(models.Model):
-    _inherit = 'account.move'
+# class AccountInvoice(models.Model):
+#     _inherit = 'account.move'
 
     # po_no = fields.Char("PO No")
 
