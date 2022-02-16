81b4091cd7ec7e8ed6907d96e0937c8f7ddf5094
libu <libukoshym@gmail.com>
2022-02-16 22:43:14 +0530

    Disable on fly Create Edit Feature

---
+++ disable_create_and_edit/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Disable Create & Create and Edit',
+    'version': '11.0.1.0.0',
+    'category': 'Tools',
+    'license': 'AGPL-3',
+    'summary': 'Disable Create & Create and Edit on all objects',
+    'description': """
+Disables Create and Edit from all the models and fields
+=========================================================
+Disable quick create on all objects of Odoo.
+
+    """,
+    'author': 'LK',
+    'website': '',
+    'depends': ['base'],
+    'installable': True,
+    'data': ['views/many2one_view.xml',]
+}
