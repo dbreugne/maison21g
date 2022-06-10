029defcf3474d3dcf1152f9b270689cc337b5bc6
labib <labib@portcities.net>
2022-06-10 16:38:43 +0700

    [ADD] asset information section

---
+++ ms_account_asset/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': "Eugene Maison - Account Asset",
+    'summary': "Customization in account asset",
+    'version': '13.0.1.0.0',
+    'description': """
+        v 1.0.0 (mfm) \n
+        - adding asset info (owner, SN, location)
+    """,
+    'author': "Portcities",
+    'website': "http://portcities.net",
+    'category': 'Accounting',
+    'depends': ['account_asset'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/account_asset_views.xml',
+        'views/asset_location_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
