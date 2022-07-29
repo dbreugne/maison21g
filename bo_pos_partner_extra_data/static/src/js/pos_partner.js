bc00f58720f998eccc0bc0ee4dbb9622aa764aca
labib <labib@portcities.net>
2022-07-29 16:29:16 +0700

    [ADD] filter parent category and segment

--- bo_pos_partner_extra_data/static/src/js/pos_partner.js
+++ bo_pos_partner_extra_data/static/src/js/pos_partner.js
@@ -12,10 +12,9 @@ models.load_fields("res.partner", ["birthdate", "gender", "married", "children"]
 models.PosModel = models.PosModel.extend({
     initialize: function(session, attributes) {
         var res = PosModelSuper.prototype.initialize.apply(this, arguments);
-        this.genders = [{'code': 'male', 'name': _t('Male')}, {'code': 'female', 'name': _t('Female')},
-        {'code': 'unisex', 'name': _t('Unisex')}];
-        this.married = [{'code': 'yes', 'name': _t('Yes')}, {'code': 'no', 'name': _t('No')}];
-        this.children =[{'code': 'yes', 'name': _t('Yes')}, {'code': 'no', 'name': _t('No')}];
+        this.genders = [{'code': 'female', 'name': _t('Female')},{'code': 'male', 'name': _t('Male')},{'code': 'unisex', 'name': _t('Unisex')}];
+        this.married = [{'code': '', 'name': ''},{'code': 'yes', 'name': _t('Yes')}, {'code': 'no', 'name': _t('No')}];
+        this.children =[{'code': '', 'name': ''},{'code': 'yes', 'name': _t('Yes')}, {'code': 'no', 'name': _t('No')}];
         return res;
     },
 });
