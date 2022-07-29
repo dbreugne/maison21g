2671bebd503d067346633c7b09c749f64d010883
labib <labib@portcities.net>
2022-07-29 16:29:16 +0700

    [ADD] filter parent category and segment

---
+++ ms_accounting_report/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': "Maison - Accounting Report",
+    'summary': "Customization in Accounting Report",
+    'version': '13.0.1.0.0',
+    'description': """
+        v 1.0.0 (mfm) \n
+        - Add filter segment, parent category in invoice analysis report \n 
+    """,
+    'author': "Portcities",
+    'website': "http://portcities.net",
+    'category': 'Sale',
+    'depends': ['account'],
+    'data': [
+        'reports/account_invoice_report_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
