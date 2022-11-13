ab3e7dc3fd98bdc15c557ce0633671c4daea9c4c
fakhry <mahzumm.fm@gmail.com>
2022-11-13 09:56:07 +0700

    [ADD] parfume configuratot mockup

---
+++ ms_pos_product_config/__manifest__.py
@@ -0,0 +1,23 @@
+
+{
+    'name': 'POS Perfume Configurator', 
+    'version': '13.0.1.1',
+    'sequence': 1, 
+    'category': 'Point Of Sale', 
+    'description': """ 
+        Add Perfume Configurator.
+    """,
+    'summary': 'Add Perfume Configurator.',
+    'author': 'Portcities Ltd',
+    'website': 'http://portcities.net',
+    'depends': ['point_of_sale'],
+    'data': [
+        'views/template.xml',
+    ],
+    'qweb': [
+        'static/src/xml/parfume_config.xml'
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
