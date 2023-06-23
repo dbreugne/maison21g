8b9449261cf1a5db5353887e4ba5c0c13c58b21e
godeliva <diva@portcities.net>
2023-06-23 15:01:53 +0700

    [ADD] create new module for inherit the account.move model

---
+++ ms_invoice/__manifest__.py
@@ -0,0 +1,23 @@
+
+{
+    'name': 'MS: Invoice',
+    'version': '13.0.1.0.0',
+    'sequence': 1,
+    'category': 'Accounting',
+    'description': """ 
+        Subtotal display in invoice form and report \n
+    """,
+    'summary': 'Invoices & Payments',
+    'author': 'Portcities Ltd',
+    'website': 'http://portcities.net',
+    'depends': ['account'],
+    'data': [
+        'reports/account_move_report.xml',
+        'views/account_move_views.xml',
+    ],
+    'qweb': [
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
