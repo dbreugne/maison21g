121b774190c0cfa749db66c502707f6919ce7400
Aneri Spellbound <aneri.spellbound@gmail.com>
2023-10-26 18:29:19 +0530

    [ADD] sss_project_task : Added new two fields in project task and one is hours and one is amount

---
+++ sss_project_task/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Project Task', 
+    'version': '13.0.1',
+    'sequence': 1, 
+    'category': 'Project Task', 
+    'description': 
+        """ 
+        Project Task.
+    """,
+    'summary': 'Project Task',
+    'author': 'Spellbound Soft Solutions',
+    'website': 'http://spellboundss.com/',
+    'depends': ['project'],
+    'data': [
+        'views/project_task_view.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
