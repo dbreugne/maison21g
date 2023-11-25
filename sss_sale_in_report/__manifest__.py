8a456621fffd5cf0854e9b212a72920c89b7fcb4
Aneri Spellbound <aneri.spellbound@gmail.com>
2023-11-25 19:27:44 +0530

    [MOD]sss_sale_in_report : Intial file

--- sss_sale_in_report/__manifest__.py
+++ sss_sale_in_report/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': 'SSS Sale In Report',
     'description': """sss_sale_in_report""",
-    'version': '13.2',
+    'version': '0.1',
     'summary': 'sss sale in report',
     'author': 'Spellbound Soft Solution',
     'website': 'http://spellboundss.com',
