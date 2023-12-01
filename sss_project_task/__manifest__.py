2d5cb66aff309933ca0683dc79c8c68bad2dc7ed
Aneri Spellbound <aneri.spellbound@gmail.com>
2023-12-01 12:37:43 +0530

    [MOD]sss_project_task : Add field and calculated hours * Amount

--- sss_project_task/__manifest__.py
+++ sss_project_task/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Project Task', 
-    'version': '13.0.3',
+    'version': '13.0.4',
     'sequence': 1, 
     'category': 'Project Task', 
     'description': 
--- sss_project_task/models/project_task.py
+++ sss_project_task/models/project_task.py
@@ -1,10 +1,25 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from odoo import fields, models, api
+
 
 class ProjectTask(models.Model):
 	_inherit = "project.task"
 
-	hours = fields.Char(string="Hours")
-	amount = fields.Float(string="Amount")
+	hours = fields.Char(string="Estimated Hours")
+	amount = fields.Float(string="Estimated Amount")
+	total_amount = fields.Float(string="Total Amount", compute="_compute_total_amount")
+
+	@api.depends('hours', 'amount')
+	def _compute_total_amount(self):
+		for rec in self:
+			rec.total_amount = 0
+			if rec.hours:
+				if ':' in rec.hours:
+					hours, minutes = map(int, rec.hours.split(":"))
+					total_hours = hours + minutes / 60.0
+					rec.total_amount = float(total_hours) * rec.amount
+
+				else:
+					rec.total_amount = float(rec.hours) * rec.amount
