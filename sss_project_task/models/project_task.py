c66954c6ae65a04dde641b1e464d4c9463f4acb3
Aneri Spellbound <aneri.spellbound@gmail.com>
2023-12-19 12:04:58 +0530

    [IMP]sss_project_task : Solve pip8

--- sss_project_task/models/project_task.py
+++ sss_project_task/models/project_task.py
@@ -5,16 +5,16 @@ from odoo import fields, models, api
 
 
 class ProjectTask(models.Model):
-	_inherit = "project.task"
+    _inherit = "project.task"
 
-	hours = fields.Char(string="Estimated Hours")
-	est_hours = fields.Float(string="Estimated Hours")
-	amount = fields.Float(string="Estimated Amount", compute="_compute_total_amount")
-	total_amount = fields.Float(string="Total Amount")
+    hours = fields.Char(string="Estimated Hours")
+    est_hours = fields.Float(string="Estimated Hours")
+    amount = fields.Float(string="Estimated Amount", compute="_compute_total_amount")
+    total_amount = fields.Float(string="Total Amount")
 
-	@api.depends('est_hours')
-	def _compute_total_amount(self):
-		for rec in self:
-			rec.amount = 0
-			if rec.est_hours:
-				rec.amount = float(rec.est_hours) * 25
+    @api.depends('est_hours')
+    def _compute_total_amount(self):
+        for rec in self:
+            rec.amount = 0
+            if rec.est_hours:
+                rec.amount = float(rec.est_hours) * 25
