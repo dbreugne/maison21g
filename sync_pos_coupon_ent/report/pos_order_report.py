cc1e0c77cf534b1112ce4c60a73106239b969faa
Aneri Spellbound <aneri.spellbound@gmail.com>
2023-12-26 10:11:24 +0530

    [IMP]sync_pos_coupon_ent : solve pep8

--- sync_pos_coupon_ent/report/pos_order_report.py
+++ sync_pos_coupon_ent/report/pos_order_report.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, fields, models, tools
+from odoo import fields, models
 
 
 class PosOrderReport(models.Model):
@@ -11,5 +11,4 @@ class PosOrderReport(models.Model):
     amount_applied_promo_total = fields.Float(string='Total Amount Applied Promotions', readonly=True)
 
     def _select(self):
-        return super(PosOrderReport, self)._select() +\
-        ", SUM(s.amount_applied_coupons_total / s.count_lines) AS amount_applied_coupons_total, SUM(s.amount_applied_promo_total / s.count_lines) AS amount_applied_promo_total"
+        return super(PosOrderReport, self)._select() + ", SUM(s.amount_applied_coupons_total / s.count_lines) AS amount_applied_coupons_total, SUM(s.amount_applied_promo_total / s.count_lines) AS amount_applied_promo_total"
