ad7358b76983b3936b551fa11324e733772c6972
Aneri Spellbound <aneri.spellbound@gmail.com>
2024-02-02 18:12:50 +0530

    [ADD]sss_account_gernal_report_analytic : Add Analytic column in Gernal ledger report

---
+++ sss_account_gernal_report_analytic/__manifest__.py
@@ -0,0 +1,20 @@
+
+{
+    'name': 'Gernal Ledger Report Account Analytic', 
+    'version': '13.0.0',
+    'sequence': 1, 
+    'category': 'Gernal Ledger Report Account Analytic', 
+    'description': 
+        """ 
+        Gernal Ledger Report Account Analytic.
+    """,
+    'summary': 'Gernal Ledger Report Account Analytic',
+    'author': 'De ',
+    'website': 'http://www.de.com',
+    'depends': ['account','account_reports','web'],
+    'data': [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
