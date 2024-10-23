4dcaed1d449ce810139005479c2b9f6f2b74c2f3
Sachin Odoo <sachin.odoo@gmail.com>
2024-10-23 15:47:31 +0530

    Add files via upload

---
+++ ms_manufacture/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': "Maison - Manufacture",
+    'summary': "Customization in Manufacture",
+    'version': '17.0.1.0.0',
+    'description': """
+        v 1.0.0 (mfm) \n
+        - Editable lot name on manufacture produce wizard \n
+    """,
+    'author': "Portcities",
+    'website': "http://portcities.net",
+    'category': 'Manufacture',
+    'depends': ['mrp'],
+    'data': [
+        #'wizards/mrp_product_produce_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
