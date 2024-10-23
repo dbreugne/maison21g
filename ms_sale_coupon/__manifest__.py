56ca32c26a355cb5d6d12089d23a770e2c1cc75d
Sachin Odoo <sachin.odoo@gmail.com>
2024-10-23 16:02:41 +0530

    Add files via upload

---
+++ ms_sale_coupon/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': "Maison - Coupon",
+    'summary': "Customization Coupon",
+    'version': '17.0.1.0.0',
+    'description': """
+        v 1.0.0 (mfm) \n
+        - Sale coupon report \n
+    """,
+    'author': "Portcities",
+    'website': "http://portcities.net",
+    'category': 'Sales',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        #'data/report_paperformat.xml',
+        #'reports/sale_coupon_report.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
